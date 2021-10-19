30c1f04e746697540f6186018056c6a20c6f2239
Arzamaskina M.A <arzamaskina.ma@mail.ru>
2021-10-19 16:01:29 +0400

    Добавлен параметризованный класс  для хранения набора объектов от интерфейса ITransport и форма для отрисовки гаражей

--- Tipper_Variant2/Tipper_Variant2/FormСar.Designer.cs
+++ Tipper_Variant2/Tipper_Variant2/FormСar.Designer.cs
@@ -34,7 +34,7 @@
             this.buttonDown = new System.Windows.Forms.Button();
             this.buttonLeft = new System.Windows.Forms.Button();
             this.buttonUp = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCreateTipper = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCars)).BeginInit();
             this.SuspendLayout();
             // 
@@ -107,22 +107,22 @@
             this.buttonUp.UseVisualStyleBackColor = true;
             this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
             // 
-            // button1
+            // buttonCreateTipper
             // 
-            this.button1.Location = new System.Drawing.Point(196, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 37);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Создать самосвал";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.buttonCreateTipperCar_Click);
+            this.buttonCreateTipper.Location = new System.Drawing.Point(196, 12);
+            this.buttonCreateTipper.Name = "buttonCreateTipper";
+            this.buttonCreateTipper.Size = new System.Drawing.Size(178, 37);
+            this.buttonCreateTipper.TabIndex = 6;
+            this.buttonCreateTipper.Text = "Создать самосвал";
+            this.buttonCreateTipper.UseVisualStyleBackColor = true;
+            this.buttonCreateTipper.Click += new System.EventHandler(this.buttonCreateTipperCar_Click);
             // 
             // FormCar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonCreateTipper);
             this.Controls.Add(this.buttonUp);
             this.Controls.Add(this.buttonLeft);
             this.Controls.Add(this.buttonDown);
@@ -144,7 +144,7 @@
         private System.Windows.Forms.Button buttonDown;
         private System.Windows.Forms.Button buttonLeft;
         private System.Windows.Forms.Button buttonUp;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCreateTipper;
     }
 }
 
