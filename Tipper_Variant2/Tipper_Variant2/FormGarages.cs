1d2f3deab0a89c5a62bc44a2637fe13b93a64067
Arzamaskina M.A <arzamaskina.ma@mail.ru>
2021-12-13 20:39:33 +0400

    Исправление сохранения(2)

--- Tipper_Variant2/Tipper_Variant2/FormGarages.cs
+++ Tipper_Variant2/Tipper_Variant2/FormGarages.cs
@@ -213,8 +213,7 @@ namespace Tipper_Variant2
             {
                 try
                 {
-                    garageCollection.LoadData(openFileDialog.FileName);
-                    garageCollection.LoadData(openFileDialog.FileName);
+                    garageCollection.SaveData(openFileDialog.FileName);
                     MessageBox.Show("Сохранение прошло успешно", "Результат",
                    MessageBoxButtons.OK, MessageBoxIcon.Information);
                     logger.Info("Сохранено в файл " + saveFileDialog.FileName);
